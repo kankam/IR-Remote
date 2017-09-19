cdc94d58281aa914e3df8289b8f7c61dda75e1d8
kankam <kanckkam@gmail.com>
2017-09-19 09:51:32 +0800

    some changes

---
+++ Arduino/ir_remote_2.1/Debug/ir_remote_2.1.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\kanck\AppData\Local\Temp\VMBuilds\ir_remote_2.1\pro_16MHzatmega328\Debug\ir_remote_2.1.cpp.o: \
+ C:\Users\kanck\AppData\Local\Temp\VMBuilds\ir_remote_2.1\pro_16MHzatmega328\Debug\ir_remote_2.1.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ C:\Users\kanck\OneDrive\Documents\Arduino\libraries\multiCameraIrControl/multiCameraIrControl.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Extensions\mbsbasw2.err\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
--- Arduino/ir_remote_2.1/ir_remote_2.1.ino
+++ Arduino/ir_remote_2.1/ir_remote_2.1.ino
@@ -9,125 +9,125 @@ int counter = 5; //Default shutter time 5s
 int rate = 200; //Default speed of digit change on Button press
 int shot = 0; //Shot count
 void setup() {
-  // Set up
-  pinMode(LEDPin[0], OUTPUT);  //A
-  pinMode(LEDPin[1], OUTPUT);  //B
-  pinMode(LEDPin[2], OUTPUT);  //C
-  pinMode(LEDPin[3], OUTPUT);  //D
-  pinMode(LEDPin[4], OUTPUT);  //E
-  pinMode(LEDPin[5], OUTPUT);  //F
-  pinMode(LEDPin[6], OUTPUT);  //G
-  pinMode(LEDPin[7], OUTPUT);  //DP
-  pinMode(UP, INPUT);  //UP
-  pinMode(DOWN, INPUT);  //DOWN
-  pinMode(segPin[0], OUTPUT);  //DIG1
-  pinMode(segPin[1], OUTPUT);  //DIG2
-  pinMode(segPin[2], OUTPUT);  //DIG3
-  pinMode(segPin[3], OUTPUT);  //DIG4
-  pinMode(START, INPUT);  //START/Stop switch for staging
+	  // Set up
+	  pinMode(LEDPin[0], OUTPUT);  //A
+	  pinMode(LEDPin[1], OUTPUT);  //B
+	  pinMode(LEDPin[2], OUTPUT);  //C
+	  pinMode(LEDPin[3], OUTPUT);  //D
+	  pinMode(LEDPin[4], OUTPUT);  //E
+	  pinMode(LEDPin[5], OUTPUT);  //F
+	  pinMode(LEDPin[6], OUTPUT);  //G
+	  pinMode(LEDPin[7], OUTPUT);  //DP
+	  pinMode(UP, INPUT);  //UP
+	  pinMode(DOWN, INPUT);  //DOWN
+	  pinMode(segPin[0], OUTPUT);  //DIG1
+	  pinMode(segPin[1], OUTPUT);  //DIG2
+	  pinMode(segPin[2], OUTPUT);  //DIG3
+	  pinMode(segPin[3], OUTPUT);  //DIG4
+	  pinMode(START, INPUT);  //START/Stop switch for staging
 }
 void DisplaySingleDigit(int digit)
 {
- //Display Single Digit on 7 segment display
- //Conditions for displaying segment a
- if(digit!=1 && digit != 4)
- digitalWrite(LEDPin[0],HIGH);
- 
- //Conditions for displaying segment b
- if(digit != 5 && digit != 6)
- digitalWrite(LEDPin[1],HIGH);
- 
- //Conditions for displaying segment c
- if(digit !=2)
- digitalWrite(LEDPin[2],HIGH);
- 
- //Conditions for displaying segment d
- if(digit != 1 && digit !=4 && digit !=7)
- digitalWrite(LEDPin[3],HIGH);
- 
- //Conditions for displaying segment e 
- if(digit == 2 || digit ==6 || digit == 8 || digit==0)
- digitalWrite(LEDPin[4],HIGH);
- 
- //Conditions for displaying segment f
- if(digit != 1 && digit !=2 && digit!=3 && digit !=7)
- digitalWrite(LEDPin[5],HIGH);
-  //Conditions for displaying segment g
- if (digit!=0 && digit!=1 && digit !=7)
- digitalWrite(LEDPin[6],HIGH);
+	 //Display Single Digit on 7 segment display
+	 //Conditions for displaying segment a
+	 if(digit!=1 && digit != 4)
+	 digitalWrite(LEDPin[0],HIGH);
+	 
+	 //Conditions for displaying segment b
+	 if(digit != 5 && digit != 6)
+	 digitalWrite(LEDPin[1],HIGH);
+	 
+	 //Conditions for displaying segment c
+	 if(digit !=2)
+	 digitalWrite(LEDPin[2],HIGH);
+	 
+	 //Conditions for displaying segment d
+	 if(digit != 1 && digit !=4 && digit !=7)
+	 digitalWrite(LEDPin[3],HIGH);
+	 
+	 //Conditions for displaying segment e 
+	 if(digit == 2 || digit ==6 || digit == 8 || digit==0)
+	 digitalWrite(LEDPin[4],HIGH);
+	 
+	 //Conditions for displaying segment f
+	 if(digit != 1 && digit !=2 && digit!=3 && digit !=7)
+	 digitalWrite(LEDPin[5],HIGH);
+	  //Conditions for displaying segment g
+	 if (digit!=0 && digit!=1 && digit !=7)
+	 digitalWrite(LEDPin[6],HIGH);
  }
  void turnOff()
 {
-  //Clear 7 segment display
-  digitalWrite(LEDPin[0],LOW);
-  digitalWrite(LEDPin[1],LOW);
-  digitalWrite(LEDPin[2],LOW);
-  digitalWrite(LEDPin[3],LOW);
-  digitalWrite(LEDPin[4],LOW);
-  digitalWrite(LEDPin[5],LOW);
-  digitalWrite(LEDPin[6],LOW);
+	  //Clear 7 segment display
+	  digitalWrite(LEDPin[0],LOW);
+	  digitalWrite(LEDPin[1],LOW);
+	  digitalWrite(LEDPin[2],LOW);
+	  digitalWrite(LEDPin[3],LOW);
+	  digitalWrite(LEDPin[4],LOW);
+	  digitalWrite(LEDPin[5],LOW);
+	  digitalWrite(LEDPin[6],LOW);
 }
 
 
 void displayDigit(int digit, int t){
-  //Display a 4 digit number (digit) for (t) ms
-  int InDigit[4]; 
-  unsigned long StartT = millis(); // Record start time of this function
-  unsigned long EndT = StartT + t; //End condition of this function
-  
-  for(int i = 3; i >= 0; i--) {
-    //Store a 4 digit number in a array
-      if(digit <= 0){
-      InDigit[i] = 0;}
-      else {InDigit[i] = digit % 10;
-      digit /= 10;}
-      }
-  while(unsigned long NowT = millis() < EndT){
-    //7 segment display take turn to display their digit respectively
-    for (int b=0; b<4; b++){
-       turnOff();
-      for(int a=0; a<4; a++){
-        digitalWrite(segPin[a],HIGH);
-        }
-        digitalWrite(segPin[b],LOW);
-      DisplaySingleDigit(InDigit[b]);
-        delay(5); //Refresh Frequency ~50Hz
-      }
-    }
-  } 
+	  //Display a 4 digit number (digit) for (t) ms
+	  int InDigit[4]; 
+	  unsigned long StartT = millis(); // Record start time of this function
+	  unsigned long EndT = StartT + t; //End condition of this function
+	  
+	  for(int i = 3; i >= 0; i--) {
+		//Store a 4 digit number in a array
+		  if(digit <= 0){
+		  InDigit[i] = 0;}
+		  else {InDigit[i] = digit % 10;
+		  digit /= 10;}
+		  }
+	  while(unsigned long NowT = millis() < EndT){
+		//7 segment display take turn to display their digit respectively
+		for (int b=0; b<4; b++){
+		   turnOff();
+		  for(int a=0; a<4; a++){
+			digitalWrite(segPin[a],HIGH);
+			}
+			digitalWrite(segPin[b],LOW);
+		  DisplaySingleDigit(InDigit[b]);
+			delay(5); //Refresh Frequency ~50Hz
+		  }
+		}
+	  } 
 
 void loop() {
 int digit = 0; //reset digit display
-  if(digitalRead(START) == LOW){
-  //for setting up the counter
-      displayDigit(counter,5);
-      if (digitalRead(UP) == LOW){
-        if(counter < 9999){
-	      counter++;
-      }
-        displayDigit(counter,rate);
-        if(rate>=5){
-          //increase rate when long press
-          rate = rate-5;
-        }
-        }
-      else if (digitalRead(DOWN) == LOW){
-       if(counter > 0){
-	       counter--;
-       }
-        displayDigit(counter,rate);
-        if(rate>=5){
-          //increase rate when long press
-          rate = rate-5;
-        }
-      }
-      else {
-        //reset when no button press
-        rate=200;
-        shot = 0;
-        }
+	  if(digitalRead(START) == LOW){
+		 //for setting up the counter
+		 displayDigit(counter,5);
+		 if (digitalRead(UP) == LOW){
+			if(counter < 9999){
+			  counter++;
+				}
+		 displayDigit(counter,rate);
+			if(rate>=5){
+			  //increase rate when long press
+			  rate = rate-5;
+				}
+			}
+		 else if (digitalRead(DOWN) == LOW){
+		   if(counter > 0){
+			   counter--;
+				}
+		 displayDigit(counter,rate);
+			if(rate>=5){
+			  //increase rate when long press
+			  rate = rate-5;
+				}
+		    }
+		  else {
+			//reset when no button press
+			rate=200;
+			shot = 0;
+			}
 
-  }
+	  }
   else if (digitalRead(START) == HIGH){ 
     //shutter bulb mode
 	
